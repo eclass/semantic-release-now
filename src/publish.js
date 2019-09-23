ce0b5a60ccfadf8f7fb911b7611546880639228e
Leonardo Gatica <lgatica@protonmail.com>
2019-09-23 17:45:18 -0300

    fix(publish): fix set alias to production deploy
    Migrate from now alias to now deploy --prod
    
    BREAKING CHANGE: From now the deployments set auto alias with --prod option. Not use subcommand alias.

--- src/publish.js
+++ src/publish.js
@@ -1,18 +1,14 @@
 'use strict'
 
-const path = require('path')
 const execa = require('execa')
-const { readJSON } = require('fs-extra')
 const getError = require('./get-error')
 
 module.exports = async (_, { cwd, env, stdout, stderr, logger }) => {
-  let deployOutput
-  const nowJson = await readJSON(path.resolve(cwd, 'now.json'))
   try {
     logger.log('Deploy app to now.sh')
     const deployResult = execa(
       'now',
-      ['deploy', '--token', process.env.NOW_TOKEN],
+      ['deploy', '--prod', '--token', process.env.NOW_TOKEN],
       { cwd, env }
     )
     deployResult.stdout.pipe(
@@ -23,30 +19,8 @@ module.exports = async (_, { cwd, env, stdout, stderr, logger }) => {
       stderr,
       { end: false }
     )
-    deployOutput = await deployResult
+    await deployResult
   } catch (err) {
     throw getError('ENOWDEPLOY')
-  }
-
-  if (nowJson.alias) {
-    try {
-      logger.log(`Assigning alias to deployment ${deployOutput.stdout}`)
-      const aliasResult = execa(
-        'now',
-        ['alias', '--token', process.env.NOW_TOKEN],
-        { cwd, env }
-      )
-      aliasResult.stdout.pipe(
-        stdout,
-        { end: false }
-      )
-      aliasResult.stderr.pipe(
-        stderr,
-        { end: false }
-      )
-      await aliasResult
-    } catch (err) {
-      throw getError('ENOWALIAS')
-    }
   }
 }
